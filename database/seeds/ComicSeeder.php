6213e7f9a79d36fce23c4abdaf6c7e690726c9d4
coszach@hotmail.it <coszach@hotmail.it>
2022-04-26 16:08:10 +0200

    create DB

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,6 +12,20 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        
+        foreach($comics as $c){
+            $comic = new Comic();
+
+            $comic->title = $c['title'];
+            $comic->description = $c['description'];
+            $comic->thumb = $c['thumb'];
+            $comic->price = $c['price'];
+            $comic->series = $c['series'];
+            $comic->sale_date = $c['sale_date'];
+            $comic->type = $c['type'];
+            
+            $comic->save();
+        }
     }
 }
