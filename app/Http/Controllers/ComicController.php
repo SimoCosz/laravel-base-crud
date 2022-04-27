07d088f759ad8cd202ba3a75aaa7af92e2bc8291
coszach@hotmail.it <coszach@hotmail.it>
2022-04-27 14:56:24 +0200

    add method update

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -69,7 +69,7 @@ class ComicController extends Controller
      */
     public function edit(Comic $comic)
     {
-        //
+        return view('comics.edit', compact('comic'));
     }
 
     /**
@@ -82,6 +82,10 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
         //
+        $data = $request->all();
+
+        $comic->update($data);
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
